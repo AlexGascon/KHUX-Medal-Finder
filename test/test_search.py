0092be6543a7cdac0ce03270f0c1fda3b79d6ef7
Alex <alexgascon.93@gmail.com>
2018-01-06 19:43:10 +0100

    Refactoring tests to mock HTTP requests

--- test/test_search.py
+++ test/test_search.py
@@ -1,4 +1,7 @@
 import unittest
+import requests_mock
+
+import json
 from src.search import *
 
 
@@ -10,11 +13,17 @@ class TestSearch(unittest.TestCase):
 
         self.assertEqual(expected_endpoint, compose_endpoint(filters))
 
-    def test_get_medals(self):
+    @requests_mock.Mocker()
+    def test_get_medals(self, mock_requests):
+        # Mocking HTTP requests to avoid making real requests
+        api_url = 'http://www.khuxbot.com/api/v1/medal?q=data&filter=%7B%22rarity%22:%206,%22direction%22:%22Upright%22,%22element%22:%22Power%22,%22cost%22:%224%22,%22tier%22:%226%22%7D'
+        api_response = json.loads(open('test/fixtures/medals_upright_power_c4_t6.json').read())
+        mock_requests.get(api_url, json=api_response)
+
         filters = {"direction": "Upright", "element": "Power", "cost": 4, "tier": 6}
         expected_medals = [
-            { "cost": 4, "defence": 5665, "direction": "Upright", "element": "Power", "hits": 12, "id": 943, "image_link": "/static/medal_images//Kings_Roar_6.png", "multiplier": "x2.10-3.28", "name": "Kings Roar", "notes": "Decreases enemy defense by one step for two turns; increases your power attack by two steps for two turns; deals more damage the further back it's set in your deck", "pullable": "Yes", "rarity": 6, "region": "na", "strength": 5753, "targets": "All", "tier": 6, "type": "Combat" },
-            { "cost": 4, "defence": 5718, "direction": "Upright", "element": "Power", "hits": 11, "id": 982, "image_link": "/static/medal_images//Toon_Sora_6.png", "multiplier": "x1.99-3.35","name": "Toon Sora","notes": "Increases the enemy's count by +1, increases your PSM attack by three steps for two turns, deals more damage the higher your HP","pullable": "No","rarity": 6,"region": "na","strength": 5756,"targets": "All","tier": 6,"type": "Combat" }
+            {"cost": 4, "defence": 5665, "direction": "Upright", "element": "Power", "hits": 12, "id": 943, "image_link": "/static/medal_images//Kings_Roar_6.png", "multiplier": "x2.10-3.28", "name": "Kings Roar", "notes": "Decreases enemy defense by one step for two turns; increases your power attack by two steps for two turns; deals more damage the further back it's set in your deck", "pullable": "Yes", "rarity": 6, "region": "na", "strength": 5753, "targets": "All", "tier": 6, "type": "Combat"},
+            {"cost": 4, "defence": 5718, "direction": "Upright", "element": "Power", "hits": 11, "id": 982, "image_link": "/static/medal_images//Toon_Sora_6.png", "multiplier": "x1.99-3.35","name": "Toon Sora","notes": "Increases the enemy's count by +1, increases your PSM attack by three steps for two turns, deals more damage the higher your HP","pullable": "No","rarity": 6,"region": "na","strength": 5756,"targets": "All","tier": 6,"type": "Combat"}
         ]
 
         self.assertListEqual(expected_medals, get_medals(filters))
