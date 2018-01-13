2202d11b829c56c8f182065af0e3b2b28855ae7f
Alex <alexgascon.93@gmail.com>
2018-01-13 20:10:25 +0100

    PEP8 style changes

--- test/test_search.py
+++ test/test_search.py
@@ -24,7 +24,7 @@ class TestSearch(unittest.TestCase):
         filters = {"direction": "Upright", "element": "Power", "cost": 4, "tier": 6}
         expected_medals = [
             {"cost": 4, "defence": 5665, "direction": "Upright", "element": "Power", "hits": 12, "id": 943, "image_link": "/static/medal_images//Kings_Roar_6.png", "multiplier": "x2.10-3.28", "name": "Kings Roar", "notes": "Decreases enemy defense by one step for two turns; increases your power attack by two steps for two turns; deals more damage the further back it's set in your deck", "pullable": "Yes", "rarity": 6, "region": "na", "strength": 5753, "targets": "All", "tier": 6, "type": "Combat"},
-            {"cost": 4, "defence": 5718, "direction": "Upright", "element": "Power", "hits": 11, "id": 982, "image_link": "/static/medal_images//Toon_Sora_6.png", "multiplier": "x1.99-3.35","name": "Toon Sora","notes": "Increases the enemy's count by +1, increases your PSM attack by three steps for two turns, deals more damage the higher your HP","pullable": "No","rarity": 6,"region": "na","strength": 5756,"targets": "All","tier": 6,"type": "Combat"}
+            {"cost": 4, "defence": 5718, "direction": "Upright", "element": "Power", "hits": 11, "id": 982, "image_link": "/static/medal_images//Toon_Sora_6.png", "multiplier": "x1.99-3.35","name": "Toon Sora","notes": "Increases the enemy's count by +1, increases your PSM attack by three steps for two turns, deals more damage the higher your HP", "pullable": "No", "rarity": 6, "region": "na", "strength": 5756, "targets": "All", "tier": 6, "type": "Combat"}
         ]
 
         self.assertListEqual(expected_medals, get_medals(filters))
