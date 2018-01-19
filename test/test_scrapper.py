2466fb1168f8f818fdac602260d9f03a86f0caf7
Alex <alexgascon.93@gmail.com>
2018-01-20 00:45:17 +0100

    Refactoring setUp

--- test/test_scrapper.py
+++ test/test_scrapper.py
@@ -15,15 +15,13 @@ class TestScrapper(unittest.TestCase):
         # Saving the requests_mock as an object to reuse it in the class
         with requests_mock.Mocker() as mock:
             self.requests_mock = mock
-            self.mock_requests()
 
-    def mock_requests(self):
-        """Mocking all the URLs we'll use on this test class"""
-        names_endpoint = 'https://www.khuxbot.com/api/v1/medal?q=names'
-        names_fixture_file = 'test/fixtures/scrapper/medal_names.json'
-        with open(names_fixture_file) as fixture:
-            names_response = json.loads(fixture.read())
-        self.requests_mock.get(names_endpoint, json=names_response)
+            # Mocking all the URLs we'll use on this test class
+            names_endpoint = 'https://www.khuxbot.com/api/v1/medal?q=names'
+            names_fixture_file = 'test/fixtures/scrapper/medal_names.json'
+            with open(names_fixture_file) as fixture:
+                names_response = json.loads(fixture.read())
+            self.requests_mock.get(names_endpoint, json=names_response)
 
     def test_get_medal_names(self):
         expected_names_file = 'test/fixtures/scrapper/medal_names.txt'
