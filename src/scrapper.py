d63cc306c0f1184dfa92d5a524b1c480cc280d2e
Alex <alexgascon.93@gmail.com>
2018-01-19 23:27:30 +0100

    Fixing tests

--- src/scrapper.py
+++ src/scrapper.py
@@ -3,7 +3,7 @@ import requests
 class Scrapper:
 
     def __init__(self):
-        self.names_endpoint = 'https://www.khuxbot.com/api/v1/medalaaa?q=names'
+        self.names_endpoint = 'https://www.khuxbot.com/api/v1/medal?q=names'
 
     def get_medal_names(self):
         response = requests.get(self.names_endpoint)
