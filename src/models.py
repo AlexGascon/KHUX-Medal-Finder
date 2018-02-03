8c84602c6e6c30ba7db147eb7cba87c05d258b4f
Alex <alexgascon.93@gmail.com>
2018-02-03 20:45:00 +0100

    PEP-8 fixes

--- src/models.py
+++ src/models.py
@@ -3,10 +3,12 @@ from peewee import *
 db = PostgresqlDatabase(database='database_name', user='postgres',
                         password='secret', host='10.1.0.9', port=5432)
 
+
 class BaseModel(Model):
     """Abstract model that we'll use to make other models inherit DB settings"""
     class Meta:
         database = db
+
 
 class Medal(BaseModel):
     cost = IntegerField()
