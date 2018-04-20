6bfe27515bfd8eeae3be9fd7c78c80c8613adfed
Alex <alexgascon.93@gmail.com>
2018-04-21 00:22:24 +0200

    Created prepare_reply_body helper and its tests.
    Both are still empty, but it lets us have an idea of what it should do

---
+++ khux_medal_finder/helpers.py
@@ -0,0 +1,4 @@
+def prepare_reply_body(medals):
+    """Given a list of medals, returns a text string containing a comment
+    reply adequately formatted to include its information. """
+    pass
---
+++ test/test_helpers.py
@@ -0,0 +1,11 @@
+import unittest
+
+class TestPrepareReplyBody(unittest.TestCase):
+    def test_if_there_are_no_medals_raises_exception(self):
+        pass
+
+    def test_if_there_is_only_one_medal_generates_the_table_correctly(self):
+        pass
+
+    def test_if_there_are_several_medals_generates_the_table_correctly(self):
+        pass
