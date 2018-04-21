a42f9944fe4d609e7d8ec6795e851ce96b3f0c78
Alex Gascon <alex.gascon@coverwallet.com>
2018-04-21 21:09:14 +0200

    Created helper methods to compose the table with medals information using Reddit format, and to print the multiplier values correctly.
    We'll use it when replying to the comments that trigger the bot.

--- khux_medal_finder/helpers.py
+++ khux_medal_finder/helpers.py
@@ -1,4 +1,25 @@
 def prepare_reply_body(medals):
     """Given a list of medals, returns a text string containing a comment
-    reply adequately formatted to include its information. """
-    pass
+    reply with a table adequately formatted"""
+    if not medals:
+        raise ValueError("The medal list is empty")
+
+    header_fields = ['Medal', 'Direction', 'Element', 'Targets', 'Multiplier',
+                     'Tier', 'Hits', 'Notes']
+    header = (f'{field_name}' for field_name in header_fields).join('|')
+    separator = (':--|' * len(header_fields)).join('|')
+
+    rows = []
+    for medal in medals:
+        attributes = [medal.name, medal.direction, medal.element, medal.targets,
+                      prepare_multiplier(medal), medal.tier, medal.hits, medal.notes]
+        row = attributes.join('|')
+        rows.append(row)
+
+    return [header, separator, *rows[:10]].join('\n')
+
+def prepare_multiplier(medal):
+    if medal.multiplier_min == medal.multiplier_max:
+        return f'x{multplier.max}'
+    else:
+        return f'x{medal.multiplier_min} - {medal.multiplier_max}'
