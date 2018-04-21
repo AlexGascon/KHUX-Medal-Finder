e2a71b79db61404aea6d60dc9303eb8740d513c5
Alex Gascon <alex.gascon@coverwallet.com>
2018-04-21 23:40:11 +0200

    Minor style changes and performance improvements

--- khux_medal_finder/helpers.py
+++ khux_medal_finder/helpers.py
@@ -4,19 +4,21 @@ def prepare_reply_body(medals):
     if not medals:
         raise ValueError("The medal list is empty")
 
-    header_fields = ['Medal', 'Direction', 'Element', 'Targets', 'Multiplier',
-                     'Tier', 'Hits', 'Notes']
-    header = '|'.join(f'{field_name}' for field_name in header_fields)
-    separator = ':--|' * len(header_fields)
+    header_fields = ('Medal', 'Direction', 'Element', 'Targets', 'Multiplier', 'Tier', 'Hits', 'Notes')
+    header = '|'.join(header_fields)
+
+    header_separator = ':--|' * len(header_fields)
 
     rows = []
-    for medal in medals:
+    # Include only 10 medals, to avoid generating a huge comment if the
+    # requirements are generic
+    for medal in medals[:10]:
         attributes = [medal.name, medal.direction, medal.element, medal.targets,
                       prepare_multiplier(medal), medal.tier, medal.hits, medal.notes]
         row = '|'.join(str(attribute) for attribute in attributes)
         rows.append(row)
 
-    return '\n'.join([header, separator, *rows[:10]])
+    return '\n'.join((header, header_separator, *rows))
 
 def prepare_multiplier(medal):
     if medal.multiplier_min == medal.multiplier_max:
